69a391c690711565620a16080596592fb424117b
nhtyy <n@push0.dev>
2024-12-18 11:43:57 -0800

    fix(perf): effciently constrain a non-decompressable point

--- curve25519-dalek/src/edwards.rs
+++ curve25519-dalek/src/edwards.rs
@@ -59,7 +59,7 @@
 //! scalar multiplication;
 //!
 //! * an implementation of the
-//! [`MultiscalarMul`](../traits/trait.MultiscalarMul.html) trait for
+//! [`MultiscalarMul`](../traits/trait.MultiscalarMul.html) trait for/// The result is either `1` if the point is outside of the field order, 
 //! constant-time variable-base multiscalar multiplication;
 //!
 //! * an implementation of the
@@ -161,19 +161,30 @@ cfg_if::cfg_if! {
             fn from(value: EdwardsPoint) -> Self {
                 let mut limbs = [0u32; 16];
 
-                // Ensure that the point is normalized.
-                assert_eq!(value.Z, FieldElement::ONE);
+                // Invert `Z` inside an unconstrained block to normalize the point.
+                sp1_lib::unconstrained! {
+                    let mut buf = value.Z.invert().as_bytes();
+                    sp1_lib::io::hint_slice(&buf);
+                }
+
+                // Check that the hint is canonical, and an inverse.
+                let z_inv = read_and_verify_canon().unwrap();
+                assert!(&z_inv * &value.Z == FieldElement::ONE, "The inverse of z is not correct. This is a bug.");
+
+                // Multiply by `z_inv` to normalize the point.
+                let value_x = &value.X * &z_inv;
+                let value_y = &value.Y * &z_inv;
 
                 // Convert the x and y coordinates to little endian u32 limbs.
                 for (x_limb, x_bytes) in limbs[..8]
                     .iter_mut()
-                    .zip(value.X.as_bytes().chunks_exact(4))
+                    .zip(value_x.as_bytes().chunks_exact(4))
                 {
                     *x_limb = u32::from_le_bytes(x_bytes.try_into().unwrap());
                 }
                 for (y_limb, y_bytes) in limbs[8..]
                     .iter_mut()
-                    .zip(value.Y.as_bytes().chunks_exact(4))
+                    .zip(value_y.as_bytes().chunks_exact(4))
                 {
                     *y_limb = u32::from_le_bytes(y_bytes.try_into().unwrap());
                 }
@@ -234,28 +245,97 @@ impl CompressedEdwardsY {
     ///
     /// Returns `None` if the input is not the \\(y\\)-coordinate of a
     /// curve point.
+    /// NOTE: In the patched version, the decompress fails when the `y` coordinate is not canonical, i.e. not less than the field modulus.
+    /// In the original crate, the decompress succeeds even when the `y` coordinate is not canonical, between `2^255 - 19` and `2^255 - 1`.
     pub fn decompress(&self) -> Option<EdwardsPoint> {
         #[cfg(all(target_os = "zkvm", target_vendor = "succinct"))]
         {
-            // Use a hook to see if we can decompress with the syscall.
+            // A field element that is *not* a square in the field.
             //
-            // Our decompression syscall requires that the compressed point has a valid square
-            // y-coordinate.
+            // This is useful becasue we want to prove that 
+            // `u_div_v` is not a square which is the case when 
+            // the point is not decompressable.
             //
-            // A malicious prover could try to use the syscall anyway, but the executor will
-            // panic, and the AIR cannot be satisfied.
+            // If α is a generator and a and b are squares then are of the form 
+            // a = α^2k and b = α^2k'.
+            //
+            // If a and b are are squares then a * b = α^2(k + k') is also a square
+            // since sqrt(a * b) is α^(k + k').
+            //
+            // If a and b are not squares, they are of the form 
+            // a = α^(2k + 1) and b = α^(2k' + 1) for some integers k and k'. 
+            // Their product a * b = α^(2(k + k') + 2) = α^(2(k + k' + 1)) is a square,
+            // because the exponent 2(k + k' + 1) is an even integer.
+            //
+            // todo!(n): this should be const, pending upstream.
+            let nqr: FieldElement = {
+                let mut nqr_bytes = [0u8; 32];
+                nqr_bytes[0] = 2;
+
+                FieldElement::from_bytes(&nqr_bytes)
+            };
+ 
+            let Y = FieldElement::from_bytes(self.as_bytes());
+            let Z = FieldElement::ONE;
+            let YY = Y.square();
+            let u = &YY - &Z;                            // u =  y²-1
+            let v = &(&YY * &constants::EDWARDS_D) + &Z; // v = dy²+1
+            
+            let mut buf = [0u8; 64];
+            buf[0..32].copy_from_slice(self.as_bytes());
+            buf[32..].copy_from_slice(&v.as_bytes());
+
+            // Use a hook to see if we can decompress with the syscall. 
             sp1_lib::unconstrained! {
-                sp1_lib::io::write(sp1_lib::io::FD_EDDECOMPRESS, self.as_bytes()); 
+                sp1_lib::io::write(sp1_lib::io::FD_EDDECOMPRESS, &buf); 
             }
             
             // Read the status of the hook.
             //
             // If the status is 1, the hook says we can use the syscall.
-            // Otherwise we must decompress the point "normally", to properly constrain the
-            // failure.
-            if sp1_lib::io::read_vec().first().expect("We should have a status from the hook") == &1 {
+            let status = sp1_lib::io::read_vec().first().cloned().expect("We should have a status from the hook");
+            if status == 1 {
                 return Some(self.decompress_with_syscall());
             }
+            
+            // This is the first assertion that the point is not decompressable.
+            //
+            // If the point is outside the field and has a non-canonical representation it cannot be decompressed.
+            // Here Y is created via `from_bytes` which does the reduction.
+            //
+            // Note: This deviates from the original implentation, which didnt require canon
+            // inputs.
+            //
+            // Mask the sign bit.
+            let mut masked = self.to_bytes();
+            masked[31] &= 0b0111_1111;
+
+            // Here the compressed form without the sign bit is compared with `Y`'s `as_bytes()`, which returns the canonical form
+            if Y.as_bytes() != masked {
+                return None;                
+            }
+
+            // The hint indicated this point is not decompressable, and its not becasue of the
+            // previous reason.
+            //
+            // The other reason a decompression could fail is that the input
+            // has a `u/v` value that is not square.
+            //
+            // we check that by confirming that the hinted root satisfies 
+            // hinted_root * hinted_root = NQR * u_div_v
+
+            // v_inv is checked to be canonical and a correct inverse.
+            let v_inv = read_and_verify_canon().unwrap();
+            assert!(&v_inv * &v == FieldElement::ONE, "The inverse of v is not correct. This is a bug.");
+            
+            // Hinted root is checked to be canonical and non-zero.
+            let hinted_root = read_and_verify_canon().unwrap();
+            assert!(!bool::from(hinted_root.is_zero()));
+            
+            // Constrain `hinted_root * hinted_root = NQR * u_div_v`
+            assert_eq!(hinted_root.square(), &(&nqr * &u) * &v_inv, "The hinted root does not satisfy the NQR check. This is a bug.");
+
+            return None;
         }
 
         let (is_valid_y_coord, X, Y, Z) = decompress::step_1(self);
@@ -291,6 +371,21 @@ impl CompressedEdwardsY {
             T: &X * &Y,
         }
     }
+}
+
+#[cfg(all(target_os = "zkvm", target_vendor = "succinct"))]
+fn read_and_verify_canon() -> Option<FieldElement> {
+    let raw_bytes: [u8; 32] = sp1_lib::io::read_vec().try_into().unwrap();
+    
+    let fe = FieldElement::from_bytes(&raw_bytes);
+
+    // Check that the read hint is canonical.
+    // Compare the hint with the result of `to_bytes`, which returns canonical form.
+    if fe.as_bytes() != raw_bytes {
+        return None;
+    }
+
+    Some(fe)
 }
 
 mod decompress {
